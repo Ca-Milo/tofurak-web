306e8f79b41003510018425f81783aabd79908b2
Camilo <ccamilosanchez.1@gmail.com>
2026-04-23 20:42:55 -0500

    fix(api): update API_BASE URL and correct server status endpoint

--- src/app/services/server-status.service.ts
+++ src/app/services/server-status.service.ts
@@ -7,7 +7,7 @@ import { API_BASE } from './api.constants';
 @Injectable({ providedIn: 'root' })
 export class ServerStatusService {
   // Construimos la URL a partir de la constante API_BASE
-  private url = `${API_BASE}/server-status`;
+  private url = `${API_BASE}/status`;
 
   constructor(private http: HttpClient) {}
 
